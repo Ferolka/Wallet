b62fdcf9d2d8ffbb31527c764ad17920dfcf409b
daryna-oliinyk <70644536+daryna-oliinyk@users.noreply.github.com>
2021-03-02 23:05:30 +0200

    fixed dependencies for auth

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,6 +15,20 @@ public class DemoApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Configuration
+    public class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests(authz -> authz
+                            .antMatchers(HttpMethod.GET, "/user/**").hasAuthority("SCOPE_read")
+                            .antMatchers(HttpMethod.POST, "/user/registration").hasAuthority("SCOPE_write")
+                            .anyRequest().authenticated())
+                    .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+        }
     }
 
 }
