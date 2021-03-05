84f091decdfff43d3119242d4c14713bf07088ea
AndryKovalenko <andry.kovalenko@gmail.com>
2021-03-05 12:50:41 +0200

    add vue config

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -17,19 +17,19 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @Configuration
-    public class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .authorizeRequests(authz -> authz
-                            .antMatchers(HttpMethod.GET, "/user/**").hasAuthority("SCOPE_read")
-                            .antMatchers(HttpMethod.POST, "/user/registration").hasAuthority("SCOPE_write")
-                            .anyRequest().authenticated())
-                    .oauth2ResourceServer(oauth2 -> oauth2.jwt());
-        }
-    }
+//    @Configuration
+//    public class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//        @Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//            http
+//                    .authorizeRequests(authz -> authz
+//                            .antMatchers(HttpMethod.GET, "/user/**").hasAuthority("SCOPE_read")
+//                            .antMatchers(HttpMethod.POST, "/user/registration").hasAuthority("SCOPE_write")
+//                            .anyRequest().authenticated())
+//                    .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+//        }
+//    }
 
 }
 //"D:\programs\mysql-8.0.19-winx64\bin\mysqld" --console
