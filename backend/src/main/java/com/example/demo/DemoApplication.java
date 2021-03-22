39178d41f3906ae3755cafc148fca2061f2de92d
daryna-oliinyk <70644536+daryna-oliinyk@users.noreply.github.com>
2021-03-22 21:39:25 +0200

    remove security

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -29,12 +29,12 @@ public class DemoApplication {
     }
 
 
-//    @Configuration
-//    public class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http .csrf().disable() .authorizeRequests() .anyRequest().permitAll();
+    @Configuration
+    public class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http .csrf().disable() .authorizeRequests() .anyRequest().permitAll();
 //            http
 //                    .authorizeRequests(authz -> authz
 //                            .antMatchers(HttpMethod.GET, "/user/**").hasAuthority("SCOPE_read")
@@ -42,9 +42,9 @@ public class DemoApplication {
 //                            .anyRequest().authenticated())
 //                    .oauth2ResourceServer(oauth2 -> oauth2.jwt());
 //
-//        }
-//
-//    }
+        }
+
+    }
 
 }
 //"D:\programs\mysql-8.0.19-winx64\bin\mysqld" --console
